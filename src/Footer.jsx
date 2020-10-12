3acd5160e2b164394070dbb6da041d12e9a74afe
Daniel Michael <daniel@daniels-air.myfiosgateway.com>
2020-10-12 12:54:45 -0400

    upd

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -22,13 +22,28 @@ const Footer = (props) => {
         {/* ABOUT */}
       
         <Link to='/About'>
-          <img
-            src="https://i.imgur.com/Zhlm6SD.png"
-            width="150"
-            height="150"
-            className="about"
-            alt="Logo"
-          />
+          {props.location.pathname === '/About' ? null :
+            <img
+              src="https://i.imgur.com/Zhlm6SD.png"
+              width="150"
+              height="150"
+              className="about"
+              alt="Logo"
+            />
+          }
+          {props.location.pathname === '/' ? null :
+            
+            <a onClick href='https://github.com/DannyMichaels/' target='_blank'>
+              <img
+              src="https://i.pinimg.com/originals/f0/b4/e1/f0b4e156fa3664144cc219f32ac67f86.png"
+                width='150'
+                height='150'
+                className='about'
+                alt='github'
+              />
+            </a>
+         
+          }
         </Link>
       </footer>
     </div>
