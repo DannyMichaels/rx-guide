b763d3de07afe642bf2c2778d958802c1ecc234a
Daniel Michael <daniel@daniels-air.myfiosgateway.com>
2020-10-12 22:19:51 -0400

    fixed router

--- src/App.js
+++ src/App.js
@@ -12,20 +12,20 @@ import Home from './Home'
 import './App.css';
 
 function App() {
-  const [meds, setMeds] = useState([])
-  const [fetchMeds, setFetchMeds] = useState(false)
-  useEffect(() => {
-    const getApi = async () => {
-      const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/prescriptions`
-      const response = await axios.get(airtableURL, {
-        headers: {
-          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`
-        }
-      })
-      setMeds(response.data.records)
-    }
-  getApi()
-  }, [fetchMeds])
+  // const [meds, setMeds] = useState([])
+  // const [fetchMeds, setFetchMeds] = useState(false)
+  // useEffect(() => {
+  //   const getApi = async () => {
+  //     const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/prescriptions`
+  //     const response = await axios.get(airtableURL, {
+  //       headers: {
+  //         Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`
+  //       }
+  //     })
+  //     setMeds(response.data.records)
+  //   }
+  // getApi()
+  // }, [fetchMeds])
  
   return (
     <>
@@ -33,16 +33,17 @@ function App() {
         <div className="App">
           <Header />
 
-          {/* <main> */}
-
-            {/* <Home /> */}
-          {/* </main> */}
-          
+          <main>
+          <Route exact path='/'>
+        <Home />  
+        </Route>
+          </main>
+{/* 
           {meds.map((med) => 
             
             <Med med={med} fetchMeds={fetchMeds} setFetchMeds={setFetchMeds}/>)}
             
-          < CreateMed meds={meds} fetchMeds={fetchMeds} setFetchMeds={setFetchMeds}/>
+          < CreateMed meds={meds} fetchMeds={fetchMeds} setFetchMeds={setFetchMeds}/> */}
           
           <Footer />
           <Switch>
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import Med from './Med'
+import CreateMed from './CreateMed'
 
 export default function Home() {
+  const [meds, setMeds] = useState([])
+  const [fetchMeds, setFetchMeds] = useState(false)
+  useEffect(() => {
+    const getApi = async () => {
+      const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/prescriptions`
+      const response = await axios.get(airtableURL, {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`
+        }
+      })
+      setMeds(response.data.records)
+    }
+  getApi()
+  }, [fetchMeds])
+
   return (
     <>
-      <h1>This is the homepage!</h1>
+      <div>
+        
+      {meds.map((med) => 
+            
+            <Med med={med} fetchMeds={fetchMeds} setFetchMeds={setFetchMeds}/>)}
+            
+          < CreateMed meds={meds} fetchMeds={fetchMeds} setFetchMeds={setFetchMeds}/>
+</div>
     </>
  )
 }
