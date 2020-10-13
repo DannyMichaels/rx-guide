50216b6f9f2602100d817e76e94e4918c1eceb2b
Daniel Michael <daniel@daniels-air.myfiosgateway.com>
2020-10-12 20:35:42 -0400

    updates

--- src/Med.jsx
+++ src/Med.jsx
@@ -1,36 +1,56 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link, withRouter } from 'react-router-dom'
 import UpdateMed from './UpdateMed'
-
+import axios from 'axios'
 const Med = (props) => {
-  const [checkbox, setCheckbox] = useState('')
+  const [deleted, setDeleted] = useState(false)
+
+  const handleDelete = async () => {
+    setDeleted(true)
+    setTimeout(async () => {
+      const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/prescriptions/${props.med.id}`
+      await axios.delete(airtableURL, {
+        headers: {
+          Authorization: `Bearer: ${process.env.REACT_APP_AIRTABLE_KEY}`,
+        },
+      });
+      props.setFetchMeds(!props.fetchMeds)
+      setDeleted(false)
+    }, 2000)
+  }
+
+
   return (
-    <div>
-      <div className='med'>
-        {/* <img src="https://i.imgur.com/1qUV2SU.png"
+
+
+    <div className='med'>
+      {/* <img src="https://i.imgur.com/1qUV2SU.png"
            width='50'
           height='50'
          className ='med-pic'
         /> */}
 
-     
-        <h3>{props.med.fields.name}</h3>
-        <img src={props.med.fields.image}
-          width='100'
-          height='50'
-        />
 
-        <p>{props.med.fields.description}</p>
-       <h4>Taken At: </h4> <h5>{props.med.fields.taken}</h5>
-        {/* <input type="checkbox" value={checkbox}
-          onChange={(e) => setCheckbox(e.target.value)} /> */}
+      <h3>{props.med.fields.name}</h3>
+      <img src={props.med.fields.image}
+        width='100'
+        height='50'
+      />
 
-        <UpdateMed med={props.med}
-          fetchMeds={props.fetchMeds}
-          setFetchMeds={props.setFetchMeds}
-        />
-      </div>
-      </div>
+      <p>{props.med.fields.description}</p>
+      <h4>Taken At: </h4> <h5>{props.med.fields.taken}</h5>
+
+      <UpdateMed med={props.med}
+        fetchMeds={props.fetchMeds}
+        setFetchMeds={props.setFetchMeds}
+
+      />
+      <button onClick={handleDelete}>{deleted ? 'deleted' : <img src="https://i.imgur.com/JRRT416.png"
+        width='20px'
+      />
+      }
+      </button>
+    </div>
   );
 };
 
