841f4d40984a20300e36420b22af15f7d2e53527
Daniel Michael <daniel@daniels-air.myfiosgateway.com>
2020-10-15 10:47:45 -0400

    added alt prop to img

--- src/Med.jsx
+++ src/Med.jsx
@@ -29,11 +29,11 @@ const Med = (props) => {
       <img src={props.med.fields.image}
         width='100'
         height='50'
+        alt="Medication Image"
       />
 
       <p>{props.med.fields.description}</p>
       {props.editable && (<div>
-        {/* <h4>Strength: </h4> <p>{props.med.fields.dose}</p> */}
         <h4>Taken At: </h4> <h5>{props.med.fields.taken}</h5>
 
         <UpdateMed med={props.med}
