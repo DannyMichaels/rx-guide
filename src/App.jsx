ebbd116cda5c79229c7077b3c6ea1b404431dbef
Daniel <itzdanielmichael@gmail.com>
2021-01-06 17:47:11 -0500

    lint

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,24 @@ import Home from "./screens/Home";
 import Custom from "./screens/Custom";
 import "./App.css";
 import MedDetail from "./screens/MedDetail";
-import Layout from './Components/shared/Layout/Layout'
-import Error from './screens/Error'
+import Layout from "./Components/shared/Layout/Layout";
+import Error from "./screens/Error";
 
 function App() {
- 
-
   return (
     <>
       <Router>
         <div className="App">
           <Layout>
             <Switch>
-              <Route exact path="/" component={Home} />              <Route path="/about" exact component={About} />
-            <Route path="/custom-medication" exact component={Custom} />
-              <Route path="/medication/:name" exact component={MedDetail}/>    
+              <Route exact path="/" component={Home} />
+              <Route path="/about" exact component={About} />
+              <Route path="/custom-medication" exact component={Custom} />
+              <Route path="/medication/:name" exact component={MedDetail} />
               <Route component={Error} />
-              <Route path="*" >
-            </Route>
+              <Route path="*"></Route>
             </Switch>
-            </Layout>
+          </Layout>
         </div>
       </Router>
     </>
