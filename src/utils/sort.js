ff8dafc897e69a75637d6894a4445f39413203ac
Daniel <itzdanielmichael@gmail.com>
2021-02-18 10:31:44 -0500

    another way to do sort.js

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,4 +1,23 @@
-const compare = (a, b, key) => {
+// const compare = (a, b, key) => {
+//   const areWords = // ignore numbers for sorting
+//     a?.fields[key].match(/^([a-z]|[A-Z])+/) &&
+//     b?.fields[key].match(/^([a-z]|[A-Z])+/);
+
+//   let name1 = a?.fields[key]?.toLowerCase();
+//   let name2 = b?.fields[key]?.toLowerCase();
+
+//   if (areWords) {
+//     if (name1 < name2) {
+//       return -1;
+//     } else if (name1 > name2) {
+//       return 1;
+//     } else {
+//       return 0;
+//     }
+//   }
+// };
+
+const compare = (key) => (a, b) => {
   const areWords = // ignore numbers for sorting
     a?.fields[key].match(/^([a-z]|[A-Z])+/) &&
     b?.fields[key].match(/^([a-z]|[A-Z])+/);
@@ -14,8 +33,13 @@ const compare = (a, b, key) => {
     } else {
       return 0;
     }
+  } else {
+    return;
   }
 };
 
-export const AZ = (arr) => arr.sort((a, b) => compare(a, b, "name"));
-export const ZA = (arr) => arr.sort((a, b) => compare(a, b, "name")).reverse();
+// export const AZ = (arr) => arr.sort((a, b) => compare(a, b, "name"));
+// export const ZA = (arr) => arr.sort((a, b) => compare(a, b, "name")).reverse();
+
+export const AZ = (arr) => arr.sort(compare("name"));
+export const ZA = (arr) => arr.sort(compare("name")).reverse();
