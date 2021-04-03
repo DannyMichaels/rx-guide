dfdbbd808849a31aac233f0c97d8e98e7959c432
Daniel <itzdanielmichael@gmail.com>
2021-04-03 13:08:06 -0400

    update CustomMedForm.jsx

--- src/Components/Forms/CustomMedForm.jsx
+++ src/Components/Forms/CustomMedForm.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+import styled from 'styled-components';
+import { MedDispatchConsumer, MedStateContext } from '../../context/medContext';
+import { createCustomMed } from '../../services/globalMeds';
 
 const Form = styled.form`
   input {
@@ -34,99 +37,155 @@ const Form = styled.form`
   }
 `;
 
-const CustomMedForm = (props) => {
-  const [imagePreview, setImagePreview] = useState("");
+class CustomMedForm extends Component {
+  constructor(props) {
+    super(props);
+    this.handleImagePreview = this.handleImagePreview.bind(this);
+    this.state = {
+      imagePreview: false,
+    };
+  }
 
-  const handleImagePreview = (e) => {
+  handleImagePreview(e) {
     e.preventDefault();
-    setImagePreview(props.image);
-  };
+    this.setState({ imagePreview: this.props.image });
+  }
 
-  return (
-    <>
-      <Form onSubmit={props.handleSubmit}>
-        <label htmlFor="name" type="text">
-          Name:
-        </label>
-        <input
-          name="name"
-          type="text"
-          placeholder="Enter Drug Name"
-          required
-          autoFocus
-          value={props.name}
-          onChange={(e) => props.handleChange(e)}
-        />
+  render() {
+    const { props } = this;
+    const { imagePreview } = this.state;
 
-        <label htmlFor="class" type="text">
-          Class:
-        </label>
-        <input
-          name="medClass"
-          type="text"
-          required
-          autoFocus
-          placeholder="Enter Drug Class"
-          value={props.medClass}
-          onChange={(e) => props.handleChange(e)}
-        />
+    return (
+      <MedDispatchConsumer>
+        {(data) => {
+          const dispatch = data;
 
-        <label htmlFor="description" type="text">
-          Description:
-        </label>
-        <textarea
-          name="description"
-          type="text"
-          className="input-description"
-          placeholder="Enter Drug Description"
-          value={props.description}
-          required
-          autoFocus
-          onChange={(e) => props.handleChange(e)}
-        />
+          const handleSubmit = async (e) => {
+            e.preventDefault();
+            const { name, medClass, description, image } = props;
 
-        <label htmlFor="image" type="text">
-          Image:
-        </label>
-        <input
-          name="image"
-          type="text"
-          placeholder="https://image.png/"
-          value={props.image}
-          required
-          autoFocus
-          onChange={(e) => props.handleChange(e)}
-        />
-        <button
-          style={{
-            backgroundColor: "white",
-            margin: "10px",
-            fontFamily: "Sansita Swashed",
-          }}
-          onClick={handleImagePreview}>
-          Preview Medication
-        </button>
-        <button>
-          <img
-            src="https://i.imgur.com/BZOV6FC.png"
-            alt="add custom medication"
-          />
-        </button>
-      </Form>
-      {imagePreview && (
-        <h2 style={{ marginTop: "50px" }}>Medication Preview:</h2>
-      )}
-      {imagePreview && (
-        <div className="med">
-          <h3>{props.name}</h3>
-          <img
-            src={imagePreview}
-            style={{ maxWidth: "50px" }}
-            alt={props.name}
-          />
-        </div>
-      )}
-    </>
-  );
-};
-export default CustomMedForm;
+            if (
+              window.confirm(`Are you sure you want to add this medication?
+    \n Name: ${name}\n Class: ${medClass} \n Description: ${description} \n Image URL: ${image}`)
+            ) {
+              alert('Medication Added!');
+            } else {
+              return;
+            }
+            const fields = {
+              name,
+              description,
+              image,
+              medClass,
+            };
+
+            const createdMed = await createCustomMed(fields);
+
+            dispatch({ type: 'CREATE_MED', payload: createdMed });
+            this.setState({
+              name: '',
+              image: '',
+              description: '',
+              medClass: '',
+            });
+
+            props.history.push('/about');
+          };
+
+          return (
+            <>
+              <Form onSubmit={handleSubmit}>
+                <label htmlFor="name" type="text">
+                  Name:
+                </label>
+                <input
+                  name="name"
+                  type="text"
+                  placeholder="Enter Drug Name"
+                  required
+                  autoFocus
+                  value={props.name}
+                  onChange={(e) => props.handleChange(e)}
+                />
+
+                <label htmlFor="class" type="text">
+                  Class:
+                </label>
+                <input
+                  name="medClass"
+                  type="text"
+                  required
+                  autoFocus
+                  placeholder="Enter Drug Class"
+                  value={props.medClass}
+                  onChange={(e) => props.handleChange(e)}
+                />
+
+                <label htmlFor="description" type="text">
+                  Description:
+                </label>
+                <textarea
+                  name="description"
+                  type="text"
+                  className="input-description"
+                  placeholder="Enter Drug Description"
+                  value={props.description}
+                  required
+                  autoFocus
+                  onChange={(e) => props.handleChange(e)}
+                />
+
+                <label htmlFor="image" type="text">
+                  Image:
+                </label>
+                <input
+                  name="image"
+                  type="text"
+                  placeholder="https://image.png/"
+                  value={props.image}
+                  required
+                  autoFocus
+                  onChange={(e) => props.handleChange(e)}
+                />
+                <button
+                  style={{
+                    backgroundColor: 'white',
+                    margin: '10px',
+                    fontFamily: 'Sansita Swashed',
+                  }}
+                  onClick={this.handleImagePreview}>
+                  Preview Medication
+                </button>
+                <button>
+                  <img
+                    src="https://i.imgur.com/BZOV6FC.png"
+                    alt="add custom medication"
+                  />
+                </button>
+              </Form>
+
+              {imagePreview && (
+                <h2 style={{ marginTop: '50px' }}>Medication Preview:</h2>
+              )}
+
+              {imagePreview && (
+                <div className="med">
+                  <h3>{props.name}</h3>
+                  <img
+                    src={imagePreview}
+                    style={{ maxWidth: '50px' }}
+                    alt={props.name}
+                  />
+                </div>
+              )}
+            </>
+          );
+        }}
+      </MedDispatchConsumer>
+    );
+  }
+}
+
+CustomMedForm.contextType = MedStateContext;
+
+export default withRouter(CustomMedForm);
--- src/screens/Custom.jsx
+++ src/screens/Custom.jsx
@@ -1,73 +1,41 @@
-import React, { useState, useContext } from "react";
-import CustomMedForm from "../Components/Forms/CustomMedForm";
-import { useHistory } from "react-router-dom";
-import { createCustomMed } from "../services/globalMeds";
-import { MedDispatchContext } from "../context/medContext";
+import React, { Component } from 'react';
+import CustomMedForm from '../Components/Forms/CustomMedForm';
 
-function Custom() {
-  const [formData, setFormData] = useState({
-    name: "",
-    image: "",
-    description: "",
-    medClass: "",
-  });
-
-  const dispatch = useContext(MedDispatchContext);
-
-  const { name, image, description, medClass } = formData;
-
-  const { push } = useHistory();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (
-      window.confirm(`Are you sure you want to add this medication?
-    \n Name: ${name}\n Class: ${medClass} \n Description: ${description} \n Image URL: ${image}`)
-    ) {
-      alert("Medication Added!");
-    } else {
-      return;
-    }
-    const fields = {
-      name,
-      description,
-      image,
-      medClass,
+class Custom extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      image: '',
+      description: '',
+      medClass: '',
     };
-
-    const createdMed = await createCustomMed(fields);
-
-    dispatch({ type: "CREATE_MED", payload: createdMed });
-    setFormData("");
-    push("/about");
-  };
-
-  const handleChange = (e) => {
+  }
+  handleChange(e) {
     const { name, value } = e.target;
-
-    setFormData((prevState) => ({
+    this.setState((prevState) => ({
       ...prevState,
       [name]: value,
     }));
-  };
+  }
 
-  return (
-    <div className="about-text">
-      <h1 style={{ textShadow: "2px 2px peachpuff", color: "black" }}>
-        Add your own custom medication!
-      </h1>
+  render() {
+    return (
+      <div className="about-text">
+        <h1 style={{ textShadow: '2px 2px peachpuff', color: 'black' }}>
+          Add your own custom medication!
+        </h1>
 
-      <CustomMedForm
-        handleSubmit={handleSubmit}
-        handleChange={handleChange}
-        name={name}
-        medClass={medClass}
-        description={description}
-        image={image}
-      />
-    </div>
-  );
+        <CustomMedForm
+          handleChange={(this.handleChange = this.handleChange.bind(this))}
+          name={this.state.name}
+          medClass={this.state.medClass}
+          description={this.state.description}
+          image={this.state.image}
+        />
+      </div>
+    );
+  }
 }
 
 export default Custom;
