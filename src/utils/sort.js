5fbf470be949c31a5237c616473df8005ca62be5
Daniel <itzdanielmichael@gmail.com>
2021-04-03 13:46:49 -0400

    update sort.js

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,19 +1,36 @@
+// const compare = (key) => (a, b) => {
+//   const areWords = // ignore numbers for sorting
+//     a?.fields[key].match(/^([a-z]|[A-Z])+/) &&
+//     b?.fields[key].match(/^([a-z]|[A-Z])+/);
+
+//   let name1 = a?.fields[key]?.toLowerCase();
+//   let name2 = b?.fields[key]?.toLowerCase();
+
+//   if (areWords) {
+//     if (name1 < name2) {
+//       return -1;
+//     } else if (name1 > name2) {
+//       return 1;
+//     } else {
+//       return 0;
+//     }
+//   }
+// };
+
 const compare = (key) => (a, b) => {
-  const areWords = // ignore numbers for sorting
-    a?.fields[key].match(/^([a-z]|[A-Z])+/) &&
-    b?.fields[key].match(/^([a-z]|[A-Z])+/);
+  let name1 = a?.fields[key].toLowerCase().replace(/^([0-9])+/, ''); // ignore numbers for sorting
 
-  let name1 = a?.fields[key]?.toLowerCase();
-  let name2 = b?.fields[key]?.toLowerCase();
+  let name2 = b?.fields[key].toLowerCase().replace(/^([0-9])+/, '');
 
-  if (areWords) {
-    if (name1 < name2) {
-      return -1;
-    } else if (name1 > name2) {
-      return 1;
-    } else {
-      return 0;
-    }
+  name1 = name1.trim().replace(/[^a-zA-Z ]/g, '');
+  name2 = name2.trim().replace(/[^a-zA-Z ]/g, '');
+
+  if (name1 < name2) {
+    return -1;
+  } else if (name1 > name2) {
+    return 1;
+  } else {
+    return 0;
   }
 };
 
