ea392bbf2e9fb557d26be894f408de230a5747a0
Daniel Michael <itzdanielmichael@gmail.com>
2021-04-04 14:14:10 -0400

    Update sort.js

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,22 +1,3 @@
-// const compare = (key) => (a, b) => {
-//   const areWords = // ignore numbers for sorting
-//     a?.fields[key].match(/^([a-z]|[A-Z])+/) &&
-//     b?.fields[key].match(/^([a-z]|[A-Z])+/);
-
-//   let name1 = a?.fields[key]?.toLowerCase();
-//   let name2 = b?.fields[key]?.toLowerCase();
-
-//   if (areWords) {
-//     if (name1 < name2) {
-//       return -1;
-//     } else if (name1 > name2) {
-//       return 1;
-//     } else {
-//       return 0;
-//     }
-//   }
-// };
-
 const compare = (key) => (a, b) => {
   let name1 = a?.fields[key]
     .toLowerCase()
